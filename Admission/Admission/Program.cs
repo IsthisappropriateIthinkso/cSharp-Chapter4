14f3d3b15713c11750253088bbd77f70db49769c
IsthisappropriateIthinkso <ag137220@students.davenport.k12.ia.us>
2018-11-29 17:11:40 -0600

    Add files via upload

---
+++ Admission/Admission/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Console;
+namespace Admission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GPA_HIGHER_AVG = 60;
+            const int GPA_LOWER_AVG = 80;
+            const int NUM_RESULTS = 5;
+            double GPAentry1;
+            double GPAentry2;
+            double GPAentry3;
+            double GPAentry4;
+            double GPAentry5;
+            double testScore;
+
+            double total;
+            double GPA_average;
+
+           
+            
+           WriteLine("What is GPA #1?");
+           GPAentry1 = double.Parse(ReadLine());        
+
+            WriteLine("What is GPA #2?");
+            GPAentry2 = double.Parse(ReadLine());
+
+            WriteLine("What is GPA #3?");
+            GPAentry3 = double.Parse(ReadLine());
+
+            WriteLine("What is GPA #4?");
+            GPAentry4 = double.Parse(ReadLine());
+           
+            WriteLine("What is GPA #5?");
+            GPAentry5 = double.Parse(ReadLine());
+
+            WriteLine("what is your test score?");
+            testScore = double.Parse(ReadLine());
+
+            
+
+            total = GPAentry1 + GPAentry2 + GPAentry3 + GPAentry4 + GPAentry5;
+            GPA_average = total / NUM_RESULTS;
+
+            if (GPA_average >= 3.0 && testScore >= GPA_HIGHER_AVG||GPA_average < 3.0 && testScore >= GPA_LOWER_AVG)
+            {
+                WriteLine("Accepted");
+                WriteLine("=========");
+                WriteLine("GPA AVERAGE: {0}", GPA_average);
+            }
+            else
+            {
+                WriteLine("reject");
+                WriteLine("=======");
+                WriteLine("GPA AVERAGE: {0}", GPA_average);
+            }
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string player1;
+            do
+            {           
+            WriteLine("Do you choose rock,paper or scissors");
+            player1 = ReadLine();
+
+            Random r = new Random();
+            int ComputerNum = r.Next(4);
+
+            if (ComputerNum == 1)
+            {
+                if (player1 == "rock")
+                {
+                    WriteLine("The computer chose rock");
+                    WriteLine("It is a tie ");
+                }
+                else if (player1 == "paper")
+                {
+                    WriteLine("The computer chose paper");
+                    WriteLine("It is a tie ");
+
+                }
+                else if (player1 == "scissors")
+                {
+                     WriteLine("The computer chose scissors");
+                     WriteLine("It is a tie ");
+                }
+                else
+                {
+                     WriteLine("You must choose rock,paper or scissors!");
+
+                }
+
+            }
+
+            else if (ComputerNum== 2)
+            {
+                if (player1 == "rock")
+                {
+                     WriteLine("The computer chose paper");
+                     WriteLine("You lost,paper beat rock");
+
+                }
+                else if (player1 == "paper")
+                {
+                     WriteLine("The computer chose scissors");
+                     WriteLine("Sorry you lose,scissors beat paper ");
+
+                }
+                else if (player1 == "scissors")
+                {
+                     WriteLine("The computer chose rock");
+                     WriteLine("Sorry you lose,rock beats scissors");
+                }
+                else
+                {
+                     WriteLine("You must choose rock,paper or scissors!");
+                }
+            }
+            else if (ComputerNum == 3)
+            {
+                if (player1 == "rock")
+                {
+                     WriteLine("The computer chose scissors");
+                     WriteLine("You win,rock beats scissors");
+
+                }
+                else if (player1 == "paper")
+                {
+                     WriteLine("The computer chose rock");
+                     WriteLine("You win,paper beats rock");
+
+                }
+                else if (player1 == "scissors")
+                {
+                     WriteLine("The computer chose paper");
+                     WriteLine("You win,scissors beat paper");
+
+                }
+                else
+                {
+                     WriteLine("You must choose rock,paper or scissors!");
+
+                }
+
+            }
+
+             ReadLine();
+            } while ( ReadLine() == "yes");
+        }
+    }
+}
